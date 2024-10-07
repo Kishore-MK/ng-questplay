e0993e77b25bedb6e3bce8d69b25c2cedc56857f
Kishore-MK <tamiltac6@gmail.com>
2024-10-07 14:41:52 +0530

    #assembly-optimization

--- campaigns/gas-optimization/assembly-optimization/contracts/Challenge.sol
+++ campaigns/gas-optimization/assembly-optimization/contracts/Challenge.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 abstract contract Challenge {
 
     /**
-     * @notice Returns a copy of the given array in a gas efficient way.
+     * @notice Returns a copy of the given array in a gas-efficient way.
      * @dev This contract will be called internally.
      * @param array The array to copy.
      * @return copy The copied array.
@@ -14,8 +14,23 @@ abstract contract Challenge {
         pure 
         returns (bytes memory copy) 
     {
+        assembly {
+            // Load the length of the array
+            let len := mload(array)
 
-         // IMPLEMENT THIS FUNCTION
+            // Allocate memory for the copy
+            copy := mload(0x40)     // Load free memory pointer
+            mstore(0x40, add(copy, add(len, 0x20))) // Update free memory pointer
+            mstore(copy, len)       // Set the length of the copy
 
+            // Copy data from source to destination in one go
+            let src := add(array, 0x20)   // Start of source array data
+            let dest := add(copy, 0x20)   // Start of destination array data
+
+            // Use calldatacopy-like functionality for efficient copying
+            for { let end := add(src, len) } lt(src, end) { src := add(src, 0x20) dest := add(dest, 0x20) } {
+                mstore(dest, mload(src))
+            }
+        }
     }
 }
